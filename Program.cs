1b17bea381ba0a9b14b1c63d59018ed6b673ec5c
Edusu123 <eduardo.c.pires@outlook.com>
2023-08-01 22:12:37 -0300

    Including connection string and configuring it.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<CommandContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
