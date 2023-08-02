c4cd9342eca96f633849256d2e1a1d797726bd51
Edusu123 <eduardo.c.pires@outlook.com>
2023-08-02 11:36:20 -0300

    Add "get by id" method.

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -17,9 +17,16 @@ public class CommandsController : ControllerBase
         return _context.CommandItems;
     }
 
-    // [HttpGet]
-    // public ActionResult<IEnumerable<string>> GetString()
-    // {
-    //     return new string[] { "this", "is", "easy" };
-    // }
+    [HttpGet("{id}")]
+    public ActionResult<Command> GetCommandItem(int id)
+    {
+        var commandItem = _context.CommandItems.Find(id);
+
+        if (commandItem == null)
+        {
+            return NotFound();
+        }
+
+        return commandItem;
+    }
 }
